e3dfc20a29b3795cf1bf64c380d56e5adecd7c0e
Matt Krause <matthew.krause@outlook.com>
2021-11-07 13:40:37 -0700

    comments with planning

--- PowerShell/GroupLookup.ps1
+++ PowerShell/GroupLookup.ps1
@@ -1,4 +1,9 @@
 <#
 PowerShell script to look up all groups in a tenant.
 
+Utilizing Microsoft Graph PowerShell SDK
 #>
+
+#Connect to Graph
+
+#look up all groups/teams in the org and pull down needed info
