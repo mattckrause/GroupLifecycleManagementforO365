c89e9d9f7a5db0468f90230931bbd0bcc1ed3070
Matt Krause <matthew.krause@outlook.com>
2021-12-07 22:51:52 -0700

    initial structur and file

---
+++ Modules/get-glmgroups/Get-GLMGroups.ps1
@@ -0,0 +1,31 @@
+﻿<#
+.Synopsis
+Gets a list of M365Groups from a tenant.
+
+.Description
+
+
+.Parameter Start
+
+
+.Parameter End
+
+
+.Parameter FirstDayOfWeek
+
+
+.Parameter HighlightDay
+
+
+.Parameter HighlightDate
+
+
+.Example
+
+
+.Example
+
+
+.Example
+
+#>
